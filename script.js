5fe7dcfc879e83412cbc9427578893ca0cbb6b84
priyanka <priyankapriya15032006@gmail.com>
2025-05-16 16:01:08 +0530

    first

---
+++ script.js
@@ -0,0 +1,39 @@
+var a=20;
+var b=10;
+console.log(a+b)
+
+var a=2;
+var b=5;
+console.log(a-b)
+
+var a=4;
+var b=2;
+console.log(a*b)
+
+var a=2;
+var b=2;
+console.log(a/b)
+console.log(a+b)
+console.log(b-b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a!=b)
+console.log(a==b)
+console.log(a*b)
+console.log(a/b)
+console.log(a+b)
+console.log(a-b)
+
+function addd(){
+    var a=10;
+    var b=30;
+    console.log(a+b);
+}
+addd()
+function add(a, b){
+    console.log(a+b)
+}
+add(20,30)
+add(50,30)
+add(10,30)
