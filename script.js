5bb2bf57f948f9475a0bcc8fa63c1aaaf7891fc9
priyanka <priyankapriya15032006@gmail.com>
2025-05-17 15:59:37 +0530

    first

--- script.js
+++ script.js
@@ -32,8 +32,114 @@ function addd(){
 }
 addd()
 function add(a, b){
-    console.log(a+b)
+    console.log(a)
 }
-add(20,30)
-add(50,30)
-add(10,30)
+add("deepika")
+
+console.log(5=="five")
+console.log(3!="three")
+console.log(5!="five")
+
+var a=10;
+var b=20;
+console.log(`${a}+${b}=${a+b}`)
+
+var n=10
+if (n%2==0) 
+{
+console.log("even numvber") 
+}
+ else 
+{
+console.log("odd number")    
+}
+
+for(n=1;n<=10;n++){var n=10
+if (n%2==0) 
+{
+console.log(`${n}is even`) 
+}
+else 
+{
+console.log(`${n} is odd`)    
+}
+}
+
+for(n=1;n<=10;n++){var n=10
+if (n%2==0) 
+{
+console.log("even") 
+}
+else 
+{
+console.log("odd")    
+}
+}
+
+n=1;
+while(n <=10)
+{
+if (n%2==0) {
+console.log(`${n}is even`) 
+}
+else 
+{
+console.log(`${n} is odd`)    
+}    
+n++;
+}
+
+n=1;
+do{
+    if(n%2===0){
+        console.log(`${n} is even`)
+    }
+    else{
+        console.log(`${n} is odd`)
+    }
+    n++;
+}while(n<=10);
+
+n=6;
+switch(n){
+    case 1:
+        console.log("sunday")
+    break;
+    case 2:
+        console.log("monday")
+    break;
+    case 3:
+        console.log("tuesday")
+    break;
+    case 4:
+        console.log("wednesday")
+    break;
+    case 5:
+        console.log("thursday")
+    break;
+    case 6:
+        console.log("friday")
+    break;
+    case 7:
+        console.log("saturday")
+        break;
+    default:
+                console.log("enter the valid number")
+}
+
+var sum=()=>{
+    console.log(10+20)
+}
+sum()
+
+n=10
+var result=(n%2===0)?"even":"odd"
+console.log(result)
+var sum1=0
+for(n=1;n<10;n++){
+if(n%2===0){
+sum1=sum1+n;
+}
+}
+console.log("sum of even num from 1 to 10",sum1); 
+
